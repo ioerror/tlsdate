0809df1a75dbbe34b97c5e1105e63b83aa5d7173
David Goulet <dgoulet@ev0ke.net>
2012-07-31 23:27:34 -0400

    Add libcompat integrating clocks
    First, this patch changes gettimeofday and settimeofday function call by
    clock_gettime and clock_settime using the CLOCK_REALTIME id. The reason
    for that is for POSIX compliance and portability.
    
    Thinking ahead for a possible Windows port, this patch also introduce a
    basic compatibility layer where generic function can be used which
    points to the right OS code. A libcompat is available which for now only
    provide an interface for clocks.
    
    Refer to src/compat/clock.h for the possible calls and options. The
    tlsdate-helper was refactored to use this libcompat and new clock calls.
    
    Also silent compilation output so warnings and errors can easily be seen
    during compile time.
    
    Signed-off-by: David Goulet <dgoulet@ev0ke.net>

---
+++ src/compat/clock.h
@@ -0,0 +1,59 @@
+/* Copyright (c) 2012, David Goulet <dgoulet@ev0ke.net>
+ *                     Jacob Appelbaum
+ * Copyright (c) 2012, The Tor Project, Inc. */
+/* See LICENSE for licensing information */
+
+/**
+  * \file clock.h
+  * \brief Header file for the clock primitives.
+  **/
+
+#ifdef __linux__
+
+#include <time.h>
+
+struct tlsdate_time {
+    struct timespec tp;
+};
+
+extern int clock_get_real_time_linux(struct tlsdate_time *time);
+#define clock_get_real_time(time) clock_get_real_time_linux(time)
+
+extern int clock_set_real_time_linux(const struct tlsdate_time *time);
+#define clock_set_real_time(time) clock_set_real_time_linux(time)
+
+extern void clock_init_time_linux(struct tlsdate_time *time, time_t sec,
+                                  long nsec);
+#define clock_init_time(time, sec, nsec) \
+        clock_init_time_linux(time, sec, nsec)
+
+/* Helper macros to access time values */
+#define CLOCK_SEC(time)  ((time)->tp.tv_sec)
+#define CLOCK_MSEC(time) ((time)->tp.tv_nsec / 1000000)
+#define CLOCK_USEC(time) ((time)->tp.tv_nsec / 1000)
+#define CLOCK_NSEC(time) ((time)->tp.tv_nsec)
+
+#elif _WIN32
+
+struct tlsdate_time {
+    /* TODO: Fix Windows support */
+};
+
+extern int clock_get_real_time_win(struct tlsdate_time *time);
+#define clock_get_real_time(time) clock_get_real_time_win(time)
+
+extern int clock_set_real_time_win(const struct tlsdate_time *time);
+#define clock_set_real_time(time) clock_set_real_time_win(time)
+
+extern void clock_init_time_win(struct tlsdate_time *time, time_t sec,
+                                long nsec);
+#define clock_init_time(time, sec, nsec) \
+        clock_init_time_win(time, sec, nsec)
+
+/* Helper macros to access time values. TODO: Complete them */
+#define CLOCK_SEC(time)
+#define CLOCK_MSEC(time)
+#define CLOCK_USEC(time)
+#define CLOCK_NSEC(time)
+
+#endif /* __linux__ _WIN32 */
