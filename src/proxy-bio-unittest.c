4687c5d2e6ba965d362568f9f56239e24d76966d
Elly Fong-Jones <ellyjones@chromium.org>
2012-10-03 17:34:48 -0400

    Add proxy support
    Support SOCKS4a, SOCKS5, and HTTP proxies through -x / --proxy
    <schema>://<host>:<port>. No auth supported yet.
    
    BUG=chromium-os:31505
    TEST=unit
    
    Change-Id: Ifd430ca1ed026dcaa33a91dfa07cc7eb7e57eaee
    Signed-off-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/34566
    (cherry picked from commit 9c3e49f2f4823c822fe29342e3eebbd38154d55a)
    
    Conflicts:
    
    	src/Makefile.am
    	src/tlsdate-helper.c

---
+++ src/proxy-bio-unittest.c
@@ -0,0 +1,267 @@
+/*
+ * proxy-bio-unittest.c - proxy-bio unit tests
+ * Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <alloca.h>
+
+#include "proxy-bio.h"
+#include "test-bio.h"
+#include "test_harness.h"
+
+FIXTURE(test_bio) {
+	BIO *test;
+};
+
+FIXTURE_SETUP(test_bio) {
+	self->test = BIO_new_test();
+	ASSERT_NE(NULL, self->test);
+}
+
+FIXTURE_TEARDOWN(test_bio) {
+	BIO_free(self->test);
+}
+
+#define TEST_HOST 'w', 'w', 'w', '.', 'g', 'o', 'o', 'g', 'l', 'e', '.', \
+                  'c', 'o', 'm'
+#define TEST_HOST_SIZE 14
+static const char kTestHost[] = { TEST_HOST, 0 };
+#define TEST_PORT 80
+
+BIO *proxy_bio(BIO *test, const char *type)
+{
+	BIO *proxy = BIO_new_proxy();
+	BIO_proxy_set_type(proxy, type);
+	BIO_proxy_set_host(proxy, kTestHost);
+	BIO_proxy_set_port(proxy, TEST_PORT);
+	BIO_push(proxy, test);
+	return proxy;
+}
+
+int need_out_bytes(BIO *test, const unsigned char *out, size_t sz)
+{
+	unsigned char *buf = alloca(sz);
+	size_t i;
+	if (!buf)
+		return 1;
+	if (BIO_test_output_left(test) <  sz) {
+		fprintf(TH_LOG_STREAM, "not enough output: %d < %d\n",
+		        BIO_test_output_left(test), sz);
+		return 2;
+	}
+	if (BIO_test_get_output(test, buf, sz) != sz)
+		return 3;
+	if (memcmp(buf, out, sz)) {
+		for (i = 0; i < sz; i++) {
+			if (buf[i] != out[i])
+				fprintf(TH_LOG_STREAM,
+				        "mismatch %d %02x %02x\n", i, buf[i],
+				        out[i]);
+		}
+	}
+	return memcmp(buf, out, sz);
+}
+
+int need_out_byte(BIO *test, unsigned char out)
+{
+	unsigned char c;
+	if (BIO_test_output_left(test) < 1)
+		return 1;
+	if (BIO_test_get_output(test, &c, 1) != 1)
+		return 2;
+	return c != out;
+}
+
+int get_bytes(BIO *test, unsigned char *buf, size_t sz)
+{
+	return BIO_test_get_output(test, buf, sz);
+}
+
+void put_bytes(BIO *test, const unsigned char *buf, size_t sz)
+{
+	BIO_test_add_input(test, buf, sz);
+}
+
+void put_byte(BIO *test, char c)
+{
+	BIO_test_add_input(test, &c, 1);
+}
+
+unsigned const char kSocks4ARequest[] = {
+	0x04,	/* socks4 */
+	0x01,	/* tcp stream */
+	(TEST_PORT & 0xff00) >> 8,
+	TEST_PORT & 0xff,
+	0x00, 0x00, 0x00, 0x01,	/* bogus IP */
+	0x00,	/* userid */
+	TEST_HOST, 0x00	/* null-terminated host */
+};
+
+TEST_F(test_bio, socks4a_success)
+{
+	unsigned const char kTestInput[] = { 0xde, 0xad, 0xbe, 0xef };
+	unsigned const char kReply[] = {
+		0x00,	/* null byte */
+		0x5a,	/* success */
+		(TEST_PORT & 0xff00) >> 8,	/* port high */
+		TEST_PORT & 0xff,	/* port low */
+		0x00, 0x00, 0x00, 0x00	/* bogus IP */
+	};
+	BIO *proxy = proxy_bio(self->test, "socks4a");
+	put_bytes(self->test, kReply, sizeof(kReply));
+	EXPECT_EQ(4, BIO_write(proxy, kTestInput, sizeof(kTestInput)));
+	EXPECT_EQ(0, need_out_bytes(self->test, kSocks4ARequest,
+	                            sizeof(kSocks4ARequest)));
+	EXPECT_EQ(0, need_out_bytes(self->test, kTestInput,
+	                            sizeof(kTestInput)));
+	EXPECT_EQ(0, BIO_test_output_left(self->test));
+}
+
+TEST_F(test_bio, socks4a_fail) {
+	unsigned const char kTestInput[] = { 0xde, 0xad, 0xbe, 0xef };
+	unsigned const char kReply[] = {
+		0x00,	/* null byte */
+		0x5b,	/* fail */
+		(TEST_PORT & 0xff00) >> 8,	/* port high */
+		TEST_PORT & 0xff,	/* port low */
+		0x00, 0x00, 0x00, 0x00	/* bogus IP */
+	};
+	char buf[64];
+	BIO *proxy = proxy_bio(self->test, "socks4a");
+	put_bytes(self->test, kReply, sizeof(kReply));
+	EXPECT_EQ(0, BIO_write(proxy, kTestInput, sizeof(kTestInput)));
+	EXPECT_EQ(0, need_out_bytes(self->test, kSocks4ARequest,
+	                            sizeof(kSocks4ARequest)));
+	/* We shouldn't have written any payload */
+	EXPECT_EQ(0, BIO_test_output_left(self->test));
+}
+
+unsigned const char kSocks5AuthRequest[] = {
+	0x05,	/* socks5 */
+	0x01,	/* one auth method */
+	0x00	/* no auth */
+};
+
+unsigned const char kSocks5AuthReply[] = {
+	0x05,	/* socks5 */
+	0x00,	/* no auth */
+};
+
+unsigned const char kSocks5ConnectRequest[] = {
+	0x05,	/* socks5 */
+	0x01,	/* tcp stream */
+	0x00,	/* reserved 0x00 */
+	0x03,	/* domain name */
+	TEST_HOST_SIZE,	/* hostname with length prefix */
+	TEST_HOST,
+	(TEST_PORT & 0xff00) >> 8,
+	TEST_PORT & 0xff
+};
+
+unsigned const char kSocks5ConnectReply[] = {
+	0x05,	/* socks5 */
+	0x00,	/* success */
+	0x00,	/* reserved 0x00 */
+	0x03,	/* domain name */
+	TEST_HOST_SIZE,	/* hostname with length prefix */
+	TEST_HOST,
+	(TEST_PORT & 0xff00) >> 8,
+	TEST_PORT & 0xff
+};
+
+TEST_F(test_bio, socks5_success)
+{
+	unsigned const char kTestInput[] = { 0xde, 0xad, 0xbe, 0xef };
+	BIO *proxy = proxy_bio(self->test, "socks5");
+	put_bytes(self->test, kSocks5AuthReply, sizeof(kSocks5AuthReply));
+	put_bytes(self->test, kSocks5ConnectReply, sizeof(kSocks5ConnectReply));
+	EXPECT_EQ(4, BIO_write(proxy, kTestInput, sizeof(kTestInput)));
+	EXPECT_EQ(0, need_out_bytes(self->test, kSocks5AuthRequest,
+	                            sizeof(kSocks5AuthRequest)));
+	EXPECT_EQ(0, need_out_bytes(self->test, kSocks5ConnectRequest,
+	                            sizeof(kSocks5ConnectRequest)));
+	EXPECT_EQ(0, need_out_bytes(self->test, kTestInput,
+	                            sizeof(kTestInput)));
+	EXPECT_EQ(0, BIO_test_output_left(self->test));
+}
+
+TEST_F(test_bio, socks5_auth_fail)
+{
+	unsigned const char kTestInput[] = { 0xde, 0xad, 0xbe, 0xef };
+	unsigned const char kAuthFail[] = {
+		0x05,
+		0xff,
+	};
+	BIO *proxy = proxy_bio(self->test, "socks5");
+	put_bytes(self->test, kAuthFail, sizeof(kAuthFail));
+	EXPECT_EQ(0, BIO_write(proxy, kTestInput, sizeof(kTestInput)));
+	EXPECT_EQ(0, need_out_bytes(self->test, kSocks5AuthRequest,
+	                            sizeof(kSocks5AuthRequest)));
+	EXPECT_EQ(0, BIO_test_output_left(self->test));
+}
+
+TEST_F(test_bio, socks5_connect_fail)
+{
+	unsigned const char kTestInput[] = { 0xde, 0xad, 0xbe, 0xef };
+	unsigned const char kConnectFail[] = {
+		0x05,
+		0x01,
+		0x00,
+		0x03,
+		TEST_HOST_SIZE,
+		TEST_HOST,
+		(TEST_PORT & 0xff00) >> 8,
+		TEST_PORT & 0xff
+	};
+	BIO *proxy = proxy_bio(self->test, "socks5");
+	put_bytes(self->test, kSocks5AuthReply, sizeof(kSocks5AuthReply));
+	put_bytes(self->test, kConnectFail, sizeof(kConnectFail));
+	EXPECT_EQ(0, BIO_write(proxy, kTestInput, sizeof(kTestInput)));
+	EXPECT_EQ(0, need_out_bytes(self->test, kSocks5AuthRequest,
+	                            sizeof(kSocks5AuthRequest)));
+	EXPECT_EQ(0, need_out_bytes(self->test, kSocks5ConnectRequest,
+	                            sizeof(kSocks5ConnectRequest)));
+	EXPECT_EQ(0, BIO_test_output_left(self->test));
+}
+
+TEST_F(test_bio, http_success)
+{
+	unsigned const char kTestInput[] = { 0xde, 0xad, 0xbe, 0xef };
+	BIO *proxy = proxy_bio(self->test, "http");
+	char kConnectRequest[1024];
+	char kConnectResponse[] = "HTTP/1.0 200 OK\r\n"
+	                          "Uninteresting-Header: foobar\r\n"
+	                          "Another-Header: lol\r\n"
+	                          "\r\n";
+	snprintf(kConnectRequest, sizeof(kConnectRequest),
+	         "CONNECT %s:%d HTTP/1.1\r\n\r\n", kTestHost, TEST_PORT);
+	put_bytes(self->test, kConnectResponse, strlen(kConnectResponse));
+	EXPECT_EQ(4, BIO_write(proxy, kTestInput, sizeof(kTestInput)));
+	EXPECT_EQ(0, need_out_bytes(self->test, kConnectRequest,
+	                            strlen(kConnectRequest)));
+	EXPECT_EQ(0, need_out_bytes(self->test, kTestInput,
+	                            sizeof(kTestInput)));
+	EXPECT_EQ(0, BIO_test_output_left(self->test));
+}
+
+TEST_F(test_bio, http_error)
+{
+	unsigned const char kTestInput[] = { 0xde, 0xad, 0xbe, 0xef };
+	BIO *proxy = proxy_bio(self->test, "http");
+	char kConnectRequest[1024];
+	char kConnectResponse[] = "HTTP/1.0 403 NO U\r\n"
+	                          "Uninteresting-Header: foobar\r\n"
+	                          "Another-Header: lol\r\n"
+	                          "\r\n";
+	snprintf(kConnectRequest, sizeof(kConnectRequest),
+	         "CONNECT %s:%d HTTP/1.1\r\n\r\n", kTestHost, TEST_PORT);
+	put_bytes(self->test, kConnectResponse, strlen(kConnectResponse));
+	EXPECT_EQ(0, BIO_write(proxy, kTestInput, sizeof(kTestInput)));
+	EXPECT_EQ(0, need_out_bytes(self->test, kConnectRequest,
+	                            strlen(kConnectRequest)));
+	EXPECT_EQ(0, BIO_test_output_left(self->test));
+}
+
+TEST_HARNESS_MAIN
---
+++ src/test-bio.c
@@ -0,0 +1,155 @@
+/*
+ * test-bio.c - BIO layer for testing
+ *
+ * Copyright (c) 2012 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * This is a 'source/sink' BIO which supports synthetic inputs and outputs, and
+ * can be used to drive filter BIOs through a state machine. It buffers all
+ * output sent to it, which can be retrieved with BIO_test_get_output(), and
+ * input sent to it, which is handed back in response to BIO_read() by the
+ * filter BIO.
+ */
+
+#include <assert.h>
+#include <string.h>
+
+#include "test-bio.h"
+#include "util.h"
+
+static const unsigned int kMagic = 0x5f8d3f15;
+
+struct test_ctx {
+	unsigned int magic;
+	unsigned char *out;
+	size_t outsz;
+	unsigned char *in;
+	size_t insz;
+};
+
+static struct test_ctx *bio_ctx(BIO *b)
+{
+	struct test_ctx *ctx = b->ptr;
+	assert(ctx->magic == kMagic);
+	return ctx;
+}
+
+static size_t buf_drain(unsigned char **buf, size_t *bufsz,
+                        unsigned char *out, size_t outsz)
+{
+	if (*bufsz < outsz)
+		outsz = *bufsz;
+	memcpy(out, *buf, outsz);
+	if (*bufsz > outsz)
+		memmove(*buf, *buf + outsz, *bufsz - outsz);
+	*bufsz -= outsz;
+	*buf = realloc(*buf, *bufsz);
+	return outsz;
+}
+
+static void buf_fill(unsigned char **buf, size_t *bufsz,
+                     unsigned char *in, size_t insz)
+{
+	*buf = realloc(*buf, *bufsz + insz);
+	memcpy(*buf + *bufsz, in, insz);
+	*bufsz += insz;
+}
+
+int test_new(BIO *b)
+{
+	struct test_ctx *ctx = malloc(sizeof *ctx);
+	if (!ctx)
+		return 0;
+	ctx->magic = kMagic;
+	ctx->in = NULL;
+	ctx->insz = 0;
+	ctx->out = NULL;
+	ctx->outsz = 0;
+	b->init = 1;
+	b->flags = 0;
+	b->ptr = ctx;
+	return 1;
+}
+
+int test_free(BIO *b)
+{
+	struct test_ctx *ctx;
+	if (!b || !b->ptr)
+		return 1;
+	ctx = bio_ctx(b);
+	free(ctx->in);
+	free(ctx->out);
+	return 1;
+}
+
+int test_write(BIO *b, const char *buf, int sz)
+{
+	struct test_ctx *ctx = bio_ctx(b);
+
+	if (sz <= 0)
+		return 0;
+
+	buf_fill(&ctx->out, &ctx->outsz, (unsigned char *)buf, sz);
+	return sz;
+}
+
+int test_read(BIO *b, char *buf, int sz)
+{
+	struct test_ctx *ctx = bio_ctx(b);
+
+	if (sz <= 0)
+		return 0;
+
+	return buf_drain(&ctx->in, &ctx->insz, (unsigned char *)buf, sz);
+}
+
+long test_ctrl(BIO *b, int cmd, long num, void *ptr)
+{
+	return 0;
+}
+
+long test_callback_ctrl(BIO *b, int cmd, bio_info_cb *fp)
+{
+	return 0;
+}
+
+BIO_METHOD test_methods = {
+	BIO_TYPE_SOCKET,
+	"test",
+	test_write,
+	test_read,
+	NULL,
+	NULL,
+	test_ctrl,
+	test_new,
+	test_free,
+	test_callback_ctrl,
+};
+
+BIO_METHOD *BIO_s_test()
+{
+	return &test_methods;
+}
+
+BIO API *BIO_new_test()
+{
+	return BIO_new(BIO_s_test());
+}
+
+size_t API BIO_test_output_left(BIO *b)
+{
+	return bio_ctx(b)->outsz;
+}
+
+size_t API BIO_test_get_output(BIO *b, unsigned char *buf, size_t bufsz)
+{
+	struct test_ctx *c = bio_ctx(b);
+	return buf_drain(&c->out, &c->outsz, buf, bufsz);
+}
+
+void API BIO_test_add_input(BIO *b, unsigned char *buf, size_t bufsz)
+{
+	struct test_ctx *c = bio_ctx(b);
+	return buf_fill(&c->in, &c->insz, buf, bufsz);
+}
