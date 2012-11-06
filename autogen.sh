aa2634948c374e4ae868f22b4b3ec8130a956d86
markus reichelt <mr@mareichelt.com>
2012-11-06 21:45:34 +0100

    make autohell magic more robust on different platforms this works for me on Slackware 13.37 & Debian 6 (please test!)

--- autogen.sh
+++ autogen.sh
@@ -4,14 +4,52 @@
 # the great Makefile...
 #
 
-set -e
+echo aclocal...
+(aclocal --version) < /dev/null > /dev/null 2>&1 || {
+    echo aclocal not found
+    exit 1
+}
 
-if [ ! -d config ];
-then
-  mkdir config;
+aclocal -I ./m4 -I . ${ACLOCAL_FLAGS}
+
+echo autoheader...
+(autoheader --version) < /dev/null > /dev/null 2>&1 || {
+    echo autoheader not found
+    exit 1
+}
+
+autoheader
+
+echo -n "libtoolize... "
+if ( (glibtoolize --version) < /dev/null > /dev/null 2>&1 ); then
+    echo "using glibtoolize"
+    glibtoolize --automake --copy --force
+
+elif ( (libtoolize --version) < /dev/null > /dev/null 2>&1 ) ; then
+    echo "using libtoolize"
+    libtoolize --automake --copy --force
+
+else
+    echo "libtoolize nor glibtoolize not found"
+    exit 1
 fi
 
-WARNINGS="all,error"
-export WARNINGS
+echo automake...
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+    echo automake not found
+    exit 1
+}
 
-autoreconf --install --verbose --force
+automake --add-missing --copy --gnu
+
+echo autoconf...
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+    echo autoconf not found
+    exit 1
+}
+
+autoconf
+
+echo ready to configure
+
+exit 0
