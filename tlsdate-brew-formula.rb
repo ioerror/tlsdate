0c18558ad7d2c12bbe099d18ec6a577d849caca0
Jacob Appelbaum <jacob@appelbaum.net>
2013-02-14 01:10:44 -0800

    Update brew forumla for tlsdate

--- tlsdate-brew-formula.rb
+++ tlsdate-brew-formula.rb
@@ -1,9 +1,12 @@
 require 'formula'
 
 class Tlsdate < Formula
-  homepage 'https://github.com/ioerror/tlsdate/'
-  url 'https://github.com/ioerror/tlsdate/archive/tlsdate-0.0.6.tar.gz'
-  sha1 '<update at release time>'
+  homepage 'https://www.github.com/ioerror/tlsdate/'
+  url 'https://nodeload.github.com/ioerror/tlsdate/tar.gz/master'
+  # This hash will never be correct until we put a tagged version into master
+  # update accordingly until we do a proper release that supports OS X
+  sha1 '2e818eb327af74c6a6c86a8c8b3911e20be9bc0f'
+  version '0.0.6'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
@@ -11,7 +14,9 @@ class Tlsdate < Formula
   depends_on 'pkg-config' => :build
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./autogen.sh"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make"
     system "make install"
   end
