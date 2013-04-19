88266c5cbbeb3ed9e284913d004bfdf78581a8b3
Jacob Appelbaum <jacob@appelbaum.net>
2013-04-18 20:45:22 -0700

    Finish port of tlsdate to DragonFly BSD; tested on 3.3-DEVELOPMENT

--- src/conf.c
+++ src/conf.c
@@ -19,6 +19,10 @@
 #include "src/common/android.h" // XXX: Dirty hack - make this more generic later
 #endif
 
+#ifdef TARGET_OS_DRAGONFLYBSD
+#include "src/common/android.h" // XXX: Dirty hack - make this more generic later
+#endif
+
 #ifdef HAVE_ANDROID
 #include "src/common/android.h"
 #endif
