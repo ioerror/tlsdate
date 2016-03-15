f9d3cba7536d1679e98172ccbddad32bc9ae490c
aaf1de1e76c9425817f5045684594e6d <user@host.invalid>
2016-03-15 02:34:19 +0000

    no sslv3

--- src/tlsdate-helper.c
+++ src/tlsdate-helper.c
@@ -1133,10 +1133,6 @@ run_ssl (uint32_t *time_map, int time_is_an_illusion, int http)
   {
     verb ("V: using SSLv23_client_method()");
     ctx = SSL_CTX_new(SSLv23_client_method());
-  } else if (0 == strcmp("sslv3", protocol))
-  {
-    verb ("V: using SSLv3_client_method()");
-    ctx = SSL_CTX_new(SSLv3_client_method());
   } else if (0 == strcmp("tlsv1", protocol))
   {
     verb ("V: using TLSv1_client_method()");
